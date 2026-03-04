2df3f53c82230a43d9c0adff43f38eab24283c0c
Shubhang <materealdesigns@gmail.com>
2026-03-04 15:59:05 +0530

    release v1.0.1

--- Casks/cursor-subtitles.rb
+++ Casks/cursor-subtitles.rb
@@ -1,6 +1,6 @@
  cask "cursor-subtitles" do
-    version "1.0.0"
-    sha256 "0d75741bfa399068da98801bf771c87ad4ea7c3802c09496d473e06f3eff0997"
+    version "1.0.1"
+    sha256 "dd64f9e11c58158e03798247292e48f63397846caec026eed6f716a01c508112"
 
     url "https://github.com/shub-rajput/cursor_subtitles/releases/download/v#{version}/CursorSubtitles_v#{version}.zip"
     name "CursorSubtitles"
